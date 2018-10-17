7a936300a5ae6b924c5e3d27843fa991a2849f95
Nikita Gerasimov <n.gerasimov@rambler-co.ru>
2018-10-17 16:33:33 +0300

    inverted index search

--- t2/main.go
+++ t2/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+
+	"github.com/polis-mail-ru-golang-1/examples/t2/index"
 )
 
 func main() {
@@ -11,4 +14,35 @@ func main() {
 		return
 	}
 	fmt.Println("Reading files:", os.Args[1:])
+
+	index := index.New()
+	for _, file := range os.Args[1:] {
+		readed, err := ioutil.ReadFile(file)
+		if err != nil {
+			fmt.Printf("error reading file %q, skip\n", file)
+			continue
+		}
+		index.Add(string(readed), file)
+	}
+
+	fmt.Printf("%+v\n", index.Info())
+
+	for {
+		q := readQuery()
+		results, err := index.Search(q)
+		if err != nil {
+			fmt.Printf("error searching: %q\n", err)
+			continue
+		}
+		for _, result := range results {
+			fmt.Printf("%s -- %d\n", result.File, result.Count)
+		}
+	}
+}
+
+func readQuery() string {
+	fmt.Print("Enter query: ")
+	var in string
+	fmt.Scanln(&in)
+	return in
 }
