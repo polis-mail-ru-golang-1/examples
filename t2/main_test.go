355f4f4b6cbb82504c976de8db7d157fc0486a22
Nikita Gerasimov <n.gerasimov@rambler-co.ru>
2018-11-07 15:48:04 +0300

    remove unused benchmarks

--- t2/main_test.go
+++ t2/main_test.go
@@ -6,20 +6,8 @@ var (
 	files = []string{"books/hard", "books/noon", "books/prisoners", "books/time"}
 )
 
-func BenchmarkSingle(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		singleLoad(files)
-	}
-}
-
 func BenchmarkParallel(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		parallelLoad(files)
-	}
-}
-
-func BenchmarkMutex(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		mutexLoad(files)
 	}
 }
